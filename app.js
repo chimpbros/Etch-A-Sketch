95e550f7e6bcdf54923c30d72bb66ffa311b1d00
chimpbros <kevinnue00@gmail.com>
2023-01-17 15:26:54 +0700

    Add clear button

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-function newGrid(){
-    const container = document.querySelector('.container');
+function input(){
     let input = prompt('Specify Grids size: ');
     while (isNaN(parseInt(input)) || parseInt(input) < 1 ) {
         input = prompt("Invalid input. Please enter a number:");
     }
-    container.style.width = '256px';
-    container.style.height = '256px';
-    // create 2 nested for loops to create input x input squares of divs
-    for(let row = 0; row < input; row++){
-        for(let col = 0; col < input; col++){
+    return parseInt(input);
+}
+
+function newGrid(size){
+    for(let row = 0; row < size ** 2; row++){
             const grid = document.createElement('div');
             grid.className = 'grid';
             container.appendChild(grid);
-            grid.style.width = 256/input + 'px'
-            grid.style.height = 256/input + 'px'
-        }
+            grid.style.width = (256/size) + 'px';
+            grid.style.height = (256/size) + 'px';
     }
 }
 // change grid's background color when mouse hover
@@ -27,13 +25,19 @@ function changeColor(){
 // delete existing grid
 function rmGrid(){
     const grid = document.querySelectorAll('.grid');
-    grid.forEach(item => item.remove());
+    grid.forEach(item => item.style.backgroundColor = 'white');
 }
 
-
+const container = document.querySelector('.container');
+container.style.width = '256px';
+container.style.height = '256px';
 const createBtn = document.querySelector('.create-btn');
+const clearBtn = document.querySelector('.clear-btn');
+
 createBtn.addEventListener('click', function(){
     rmGrid();
-    newGrid();
+    let gridSize = input();
+    newGrid(gridSize);
     changeColor();
 });
+clearBtn.addEventListener('click', rmGrid);
